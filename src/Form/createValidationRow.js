5b48773834a00b861b35d7c712b6f4956b253777
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2017-01-09 13:59:34 +0200

    Remove totally useless nesting level in validation row factory

--- src/Form/createValidationRow.js
+++ src/Form/createValidationRow.js
@@ -4,29 +4,17 @@ import * as F from "../lib/func_utils"
 const errorReducer = (state, [ error, setter ]) => setter(state, error)
 const reducer = (state, list) => list.reduce(errorReducer, state)
 
-const initErrorStream = $ => (
-  $.toProperty(F.constant(undefined))
-  // Must activate manually, so it's ready to react to values from validate$ stream.
-  // Otherwise it gets first subscriber only AFTER signal from validate$
-  // – which means, that signal is actually ignored.
-  .onAny(() => {})
-)
-
 /**
  * @param validate$ Observable
  * @param config [ [Observable, function], ... ]
  * @returns {[Observable, function]}
  */
 export default (validate$, config) => {
-  const [ _streams = [], reducers = [] ] = F.zip(...config)
-  const streams = _streams.map(initErrorStream)
+  const [ streams = [], reducers = [] ] = F.zip(...config)
 
   const input =
     streams.length > 0
-      ? validate$.flatMapLatest(() =>
-          Kefir.zip(streams, (...args) => F.zip(args, reducers))
-          .take(1)
-        )
+      ? Kefir.zip(streams, (...args) => F.zip(args, reducers))
       // even if form has not fields at all,
       // it still should emit it's current state in response on validation
       : validate$.map(F.constant([]))
