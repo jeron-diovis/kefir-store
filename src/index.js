ca54827ea9543375e0ed6d020a7ddaa777fa2020
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2017-02-08 18:37:50 +0200

    export Subject from top module

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ export { default as Stream } from "./Stream"
 export { default as Model } from "./Model"
 export { default as Form } from "./Form"
 
+export { default as Subject } from "./lib/Subject"
+
 export { setConfig } from "./config"
