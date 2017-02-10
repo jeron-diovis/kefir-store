df1b392936b6e5f854bf82447cb4d64b663a314c
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2017-02-10 18:05:35 +0200

    use "lodash/fp/set" as default reducer in config

--- src/config/default.js
+++ src/config/default.js
@@ -1,9 +1,11 @@
+import set from "lodash/fp/set"
+
 export default {
   getEmptyObject: () => ({}),
   getValuesList: obj => Object.keys(obj).map(key => obj[key]),
 
   getter: prop => state => state[prop],
-  reducer: prop => (state, value) => Object.assign({}, state, { [prop]: value }),
+  reducer: prop => (state, value) => set(prop, value, state),
 
   isNotValidationError: x => x == null,
 }
