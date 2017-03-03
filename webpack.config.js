6afb022e44cf3d3ad55d61cd3985d9d550ff1cc8
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2017-03-03 18:34:13 +0200

    use parallel-webpack for creating normal and minified builds

--- webpack.config.js
+++ webpack.config.js
@@ -6,74 +6,82 @@ var fs = require("fs")
 var webpack = require("webpack")
 var WebpackErrorNotificationPlugin = require("webpack-error-notification")
 
-var isProd = process.env.NODE_ENV === "production"
+var createVariants = require("parallel-webpack").createVariants;
 
-module.exports = {
-  context: srcRoot,
-
-  entry: "index.js",
-
-  output: {
-    path: sysPath.join(root, "dist"),
-    filename: "kefir-store" + (isProd ? ".min" : "") + ".js",
-    library: "KefirStore",
-    libraryTarget: "umd",
+module.exports = createVariants(
+  {
+    minified: [ true, false ],
   },
 
-  resolve: {
-    root: srcRoot,
-  },
+  options => ({
+    context: srcRoot,
 
-  eslint: {
-    failOnError: true,
-    failOnWarning: true,
-  },
+    entry: "index.js",
 
-  externals: (
-    Object.keys(
-      JSON.parse(
-        fs.readFileSync("package.json")
-      ).dependencies
-    ).concat(
-      /^lodash($|\/.+)/
-    )
-  ),
+    output: {
+      path: sysPath.join(root, "dist"),
+      filename: "kefir-store" + (options.minified ? ".min" : "") + ".js",
+      library: "KefirStore",
+      libraryTarget: "umd",
+    },
 
-  module: {
-    preLoaders: [
-      {
-        test: /\.js$/i,
-        include: srcRoot,
-        loader: "eslint",
-      }
-    ],
+    resolve: {
+      root: srcRoot,
+    },
 
-    loaders: [
-      {
-        test: /\.js$/i,
-        include: srcRoot,
-        loader: "babel",
-      }
-    ]
-  },
+    eslint: {
+      failOnError: true,
+      failOnWarning: true,
+    },
 
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    }),
-  ].concat(
-    !isProd ? [
-      new WebpackErrorNotificationPlugin(),
-    ] : [
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.OccurrenceOrderPlugin(true),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
+    externals: (
+      Object.keys(
+        JSON.parse(
+          fs.readFileSync("package.json")
+        ).dependencies
+      ).concat(
+        /^lodash($|\/.+)/
+      )
+    ),
+
+    module: {
+      preLoaders: [
+        {
+          test: /\.js$/i,
+          include: srcRoot,
+          loader: "eslint",
         }
-      }),
-    ]
-  ),
+      ],
 
-  devtool: false,
-}
+      loaders: [
+        {
+          test: /\.js$/i,
+          include: srcRoot,
+          loader: "babel",
+        }
+      ]
+    },
+
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      }),
+    ].concat(
+      !options.minified
+        ? [
+          new WebpackErrorNotificationPlugin(),
+        ]
+        : [
+          new webpack.optimize.DedupePlugin(),
+          new webpack.optimize.OccurrenceOrderPlugin(true),
+          new webpack.optimize.UglifyJsPlugin({
+            compress: {
+              warnings: false
+            }
+          }),
+        ]
+    ),
+
+    devtool: false,
+  })
+)
