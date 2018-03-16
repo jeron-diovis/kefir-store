014a14b17cd626231e1124072c97859407b146bc
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2018-03-16 21:26:36 +0200

    rm unused code

--- src/Form/helpers/validatedOn.js
+++ src/Form/helpers/validatedOn.js
@@ -4,22 +4,12 @@ import { ap } from "../../lib/stream_utils"
 import toStream from "./toStream"
 
 const getIsValid = F.flow(F.prop("status"), F.prop("isValid"))
-const getIsValidated = F.flow(F.prop("status"), F.prop("isValidated"))
 const getValidator = F.flow(F.prop("handlers"), F.prop("validate"))
 const getState = F.prop("state")
 
 
 export const validatedOn = F.curry((form, event$) => {
-  const form$ = toStream(form)
-
-  return ap(form$.map(getValidator), event$).flatMap(Kefir.fromPromise).spy()
-
-  const isValidated$ = form$.changes().filter(getIsValidated)
-  return (
-    event$
-      .flatMapLatest(() => isValidated$.take(1))
-      .zip(ap(form$.map(getValidator), event$), F.id)
-  )
+  return ap(toStream(form).map(getValidator), event$).flatMap(Kefir.fromPromise).spy()
 })
 
 
